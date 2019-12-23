9976b91d84096a1c2473bd02933b8c16fdde531e
markee <markstephens@idrsolutions.com>
2019-12-23 11:19:38 +0000

    Sample code for DawsCon

---
+++ dawscon2020/src/dawscon2020/Dawscon2020.java
@@ -0,0 +1,21 @@
+/*
+ * Simple code example
+ */
+
+package dawscon2020;
+
+/**
+ *
+ * @author markee
+ */
+public class Dawscon2020 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Dawson");
+    }
+
+}
